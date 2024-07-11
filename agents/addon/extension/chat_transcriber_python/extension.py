83d65a4fb8dce912394d21c6550e60818fdd6f05
sunshinexcode <24xinhui@163.com>
2024-07-11 11:31:32 +0800

    feat(): add chat transcriber python extension

---
+++ agents/addon/extension/chat_transcriber_python/extension.py
@@ -0,0 +1,169 @@
+#
+#
+# Agora Real Time Engagement
+# Created by XinHui Li in 2024-07.
+# Copyright (c) 2024 Agora IO. All rights reserved.
+#
+#
+
+import logging
+from rte_runtime_python import (
+    Addon,
+    Extension,
+    register_addon_as_extension,
+    Rte,
+    Cmd,
+    Data,
+    StatusCode,
+    CmdResult,
+    MetadataInfo,
+)
+import time
+import logging
+from pb import chat_text_pb2 as pb
+
+TEXT_DATA_TEXT_FIELD = "text"
+TEXT_DATA_FINAL_FIELD = "is_final"
+TEXT_DATA_STREAM_ID_FIELD = "stream_id"
+TEXT_DATA_END_OF_SEGMENT_FIELD = "end_of_segment"
+
+CMD_NAME_FLUSH = "flush"
+
+# record the cached text data for each stream id
+cached_text_map = {}
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(process)d - [%(filename)s:%(lineno)d] - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    encoding="utf-8",
+)
+
+
+class ChatTranscriberExtension(Extension):
+    def on_init(self, rte: Rte, manifest: MetadataInfo, property: MetadataInfo) -> None:
+        logging.info("on_init")
+        rte.on_init_done(manifest, property)
+
+    def on_start(self, rte: Rte) -> None:
+        logging.info("on_start")
+        rte.on_start_done()
+
+    def on_stop(self, rte: Rte) -> None:
+        logging.info("on_stop")
+        rte.on_stop_done()
+
+    def on_deinit(self, rte: Rte) -> None:
+        logging.info("on_deinit")
+        rte.on_deinit_done()
+
+    def on_cmd(self, rte: Rte, cmd: Cmd) -> None:
+        logging.info("on_cmd")
+        cmd_json = cmd.to_json()
+        logging.info("on_cmd json: " % cmd_json)
+
+        cmd_result = CmdResult.create(StatusCode.OK)
+        cmd_result.set_property_string("detail", "success")
+        rte.return_result(cmd_result, cmd)
+
+    def on_data(self, rte: Rte, data: Data) -> None:
+        """
+        OnData receives data from rte graph.
+        current supported data:
+          - name: text_data
+            example:
+            {"name": "text_data", "properties": {"text": "hello", "is_final": true, "stream_id": 123, "end_of_segment": true}}
+        """
+        try:
+            text = data.get_property_string(TEXT_DATA_TEXT_FIELD)
+        except Exception as e:
+            logging.warning(
+                f"on_data get_property_string {TEXT_DATA_TEXT_FIELD} error: {e}"
+            )
+            return
+
+        try:
+            final = data.get_property_bool(TEXT_DATA_FINAL_FIELD)
+        except Exception as e:
+            logging.warning(
+                f"on_data get_property_bool {TEXT_DATA_FINAL_FIELD} error: {e}"
+            )
+            return
+
+        try:
+            stream_id = data.get_property_int(TEXT_DATA_STREAM_ID_FIELD)
+        except Exception as e:
+            logging.warning(
+                f"on_data get_property_int {TEXT_DATA_STREAM_ID_FIELD} error: {e}"
+            )
+            return
+
+        try:
+            end_of_segment = data.get_property_bool(TEXT_DATA_END_OF_SEGMENT_FIELD)
+        except Exception as e:
+            logging.warning(
+                f"on_data get_property_bool {TEXT_DATA_END_OF_SEGMENT_FIELD} error: {e}"
+            )
+            return
+
+        logging.debug(
+            f"on_data {TEXT_DATA_TEXT_FIELD}: {text} {TEXT_DATA_FINAL_FIELD}: {final} {TEXT_DATA_STREAM_ID_FIELD}: {stream_id} {TEXT_DATA_END_OF_SEGMENT_FIELD}: {end_of_segment}"
+        )
+
+        # We cache all final text data and append the non-final text data to the cached data
+        # until the end of the segment.
+        if end_of_segment:
+            if stream_id in cached_text_map:
+                text = cached_text_map[stream_id] + text
+                del cached_text_map[stream_id]
+        else:
+            if final:
+                if stream_id in cached_text_map:
+                    text = cached_text_map[stream_id] + text
+
+                cached_text_map[stream_id] = text
+
+        pb_text = pb.Text(
+            uid=stream_id,
+            data_type="transcribe",
+            texttime=int(time.time() * 1000),  # Convert to milliseconds
+            words=[
+                pb.Word(
+                    text=text,
+                    is_final=end_of_segment,
+                ),
+            ],
+        )
+
+        try:
+            pb_serialized_text = pb_text.SerializeToString()
+        except Exception as e:
+            logging.warning(f"on_data SerializeToString error: {e}")
+            return
+
+        try:
+            # convert the origin text data to the protobuf data and send it to the graph.
+            rte_data = data.create("data")
+            rte_data.set_property_string("data", pb_serialized_text)
+        except Exception as e:
+            logging.warning(f"on_data new_data error: {e}")
+            return
+
+        rte.send_data(rte_data)
+
+
+@register_addon_as_extension("chat_transcriber_python")
+class ChatTranscriberExtensionAddon(Addon):
+    def on_init(self, rte: Rte, manifest, property) -> None:
+        logging.info("on_init")
+        rte.on_init_done(manifest, property)
+        return
+
+    def on_create_instance(self, rte: Rte, addon_name: str) -> Extension:
+        logging.info("on_create_instance")
+        return ChatTranscriberExtension(addon_name)
+
+    def on_deinit(self, rte: Rte) -> None:
+        logging.info("on_deinit")
+        rte.on_deinit_done()
+        return
