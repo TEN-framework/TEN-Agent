5694a11af44323b39ddaeab13e435b75ba0c7158
zhangqianze <zhangqianze@agora.io>
2024-07-16 22:25:32 +0800

    tmp remove greetings from openai block

--- agents/addon/extension/openai_chatgpt_python/openai_chatgpt_extension.py
+++ agents/addon/extension/openai_chatgpt_python/openai_chatgpt_extension.py
@@ -172,15 +172,15 @@ class OpenAIChatGPTExtension(Extension):
             logger.info(f"newOpenaiChatGPT failed, err: {err}")
 
         # Send greeting if available
-        if greeting:
-            try:
-                output_data = Data.create("text_data")
-                output_data.set_property_string(DATA_OUT_TEXT_DATA_PROPERTY_TEXT, greeting)
-                output_data.set_property_bool(DATA_OUT_TEXT_DATA_PROPERTY_TEXT_END_OF_SEGMENT, True)
-                rte.send_data(output_data)
-                logger.info(f"greeting [{greeting}] sent")
-            except Exception as err:
-                logger.info(f"greeting [{greeting}] send failed, err: {err}")
+        # if greeting:
+        #     try:
+        #         output_data = Data.create("text_data")
+        #         output_data.set_property_string(DATA_OUT_TEXT_DATA_PROPERTY_TEXT, greeting)
+        #         output_data.set_property_bool(DATA_OUT_TEXT_DATA_PROPERTY_TEXT_END_OF_SEGMENT, True)
+        #         rte.send_data(output_data)
+        #         logger.info(f"greeting [{greeting}] sent")
+        #     except Exception as err:
+        #         logger.info(f"greeting [{greeting}] send failed, err: {err}")
         rte.on_start_done()
 
     def on_stop(self, rte: Rte) -> None:
