7ee9f23ed33cc83edf202ef24f3ab681e50be5e1
Jay Zhang <wangyoucao577@gmail.com>
2024-07-31 22:47:46 +0800

    fix: tts no response

--- agents/addon/extension/cosy_tts/cosy_tts_extension.py
+++ agents/addon/extension/cosy_tts/cosy_tts_extension.py
@@ -206,8 +206,9 @@ class CosyTTSExtension(Extension):
                             callback=callback,
                         )
 
-                    logger.info("on message %s", inputText)
+                    logger.info("on message [%s]", inputText)
                     tts.streaming_call(inputText)
+                    tts.streaming_complete()
                 except Exception as e:
                     logger.exception(e)
                     logger.exception(traceback.format_exc())
