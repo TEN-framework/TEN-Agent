d4360af1f69e3fb84743f9cccbd1726f20d3052d
EC <2340896+cyfyifanchen@users.noreply.github.com>
2024-10-10 13:00:56 +0800

    fix: adding the loading state for the Join btn on the landing page. (#315)
    * Adding a loading state of Join btn

--- demo/src/components/loginCard/index.tsx
+++ demo/src/components/loginCard/index.tsx
@@ -1,14 +1,15 @@
 "use client"
 
-import packageData from "../../../package.json"
+import type React from 'react'; 
 import { useRouter } from 'next/navigation'
 import { message } from "antd"
-import { useState } from "react"
-import { GithubIcon, LogoIcon } from "../icons"
+import { useState, useEffect } from "react"
 import { GITHUB_URL, getRandomUserId, useAppDispatch, getRandomChannel } from "@/common"
 import { setOptions } from "@/store/reducers/global"
 import styles from "./index.module.scss"
 
+import { GithubIcon } from "../icons"
+import packageData from "../../../package.json"
 
 const { version } = packageData
 
@@ -16,6 +17,7 @@ const LoginCard = () => {
   const dispatch = useAppDispatch()
   const router = useRouter()
   const [userName, setUserName] = useState("")
+  const [isLoadingSuccess, setIsLoadingSuccess] = useState(false);
 
   const onClickGithub = () => {
     if (typeof window !== "undefined") {
@@ -23,17 +25,28 @@ const LoginCard = () => {
     }
   }
 
-  const onUserNameChange = (e: any) => {
+  const onUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let value = e.target.value
     value = value.replace(/\s/g, "");
     setUserName(value)
   }
 
+  useEffect(() => {
+    const onPageLoad = () => {
+      setIsLoadingSuccess(true);
+    };
 
+    if (document.readyState === 'complete') {
+      onPageLoad();
+    } else {
+      window.addEventListener('load', onPageLoad, false);
+      return () => window.removeEventListener('load', onPageLoad);
+    }
+  }, []);
 
   const onClickJoin = () => {
     if (!userName) {
-      message.error("please input user name")
+      message.error("please enter a user name")
       return
     }
     const userId = getRandomUserId()
@@ -45,11 +58,20 @@ const LoginCard = () => {
     router.push("/home")
   }
 
-
   return <div className={styles.card}>
     <section className={styles.top}>
-      <span className={styles.github} onClick={onClickGithub}>
-        <GithubIcon></GithubIcon>
+      <span 
+        className={styles.github} 
+        onClick={onClickGithub}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            onClickGithub();
+          }
+        }}
+        role="button"
+        tabIndex={0}
+      >
+        <GithubIcon />
         <span className={styles.text}>GitHub</span>
       </span>
     </section>
@@ -59,19 +81,28 @@ const LoginCard = () => {
         <span className={styles.text}>The World's First Multimodal AI Agent with the OpenAI Realtime API (Beta)</span>
       </div>
       <div className={styles.section}>
-        <input placeholder="User Name" value={userName} onChange={onUserNameChange} ></input>
+        <input placeholder="User Name" value={userName} onChange={onUserNameChange} />
       </div>
       <div className={styles.section}>
-        <div className={styles.btn} onClick={onClickJoin}>
-          <span className={styles.btnText}>Join</span>
+        <div 
+          className={`${styles.btn} ${!isLoadingSuccess && styles.btnLoadingBg}`} 
+          onClick={onClickJoin}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+              onClickJoin();
+            }
+          }}
+          role="button"
+          tabIndex={0}
+        >
+          <div className={styles.btnText}>
+            {isLoadingSuccess ? "Join" : <div><span>Loading</span><span className={styles.dotting} /></div>}
+          </div>
         </div>
       </div>
       <div className={styles.version}>Version {version}</div>
     </section >
   </div >
-
-
-  return
 }
 
 export default LoginCard
