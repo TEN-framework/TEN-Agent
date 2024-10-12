5683faf488324c43b72505cebf8aa31ddd7eb4eb
caitengwen <caitengwen@agora.io>
2024-10-12 19:37:12 +0800

    Update extension.py
    1. separate FashionAIClient
    2. add try catch to asyncio.run_coroutine_threadsafe when timeout
    3. define attributes in class directly

--- agents/ten_packages/extension/fashionai/src/extension.py
+++ agents/ten_packages/extension/fashionai/src/extension.py
@@ -15,25 +15,21 @@ from ten import (
     CmdResult,
     Data,
 )
+
 from .log import logger
 import asyncio
-import json
-import ssl
-import queue
-
-import websockets
+from .fashionai_client import FashionAIClient
 import threading
 from datetime import datetime
 
 class FashionAIExtension(Extension):
-
+    app_id = ""
+    token = ""
+    channel = ""
+    stream_id = 0
 
     def on_init(self, ten_env: TenEnv) -> None:
         logger.info("FASHION_AI on_init *********************************************************")
-        self.app_id = ""
-        self.token = ""
-        self.channel = ""
-        self.stream_id = ""
         self.stopped = False
         self.queue = asyncio.Queue(maxsize=3000)
         self.client = FashionAIClient("wss://ingress.service.fasionai.com/websocket/node7/server1")
@@ -46,13 +42,16 @@ class FashionAIExtension(Extension):
         logger.info("FASHION_AI on_start *********************************************************")
 
         # TODO: read properties, initialize resources
-        self.app_id = ten_env.get_property_string("app_id")
-        self.token = ten_env.get_property_string("token")
-        self.channel = ten_env.get_property_string("channel")
-        self.stream_id = str(ten_env.get_property_int("stream_id"))
-        if len(self.token) > 0 :
-            self.app_id = self.token
-        logger.info(f"FASHION_AI on_start: app_id = {self.app_id}, token = {self.token}, channel = {self.channel}, stream_id = {self.stream_id}")
+        try:
+            self.app_id = ten_env.get_property_string("app_id")
+            self.token = ten_env.get_property_string("token")
+            self.channel = ten_env.get_property_string("channel")
+            self.stream_id = str(ten_env.get_property_int("stream_id"))
+            if len(self.token) > 0 :
+                self.app_id = self.token
+            logger.info(f"FASHION_AI on_start: app_id = {self.app_id}, token = {self.token}, channel = {self.channel}, stream_id = {self.stream_id}")
+        except Exception as e:
+            logger.warning(f"get_property err: {e}")
 
         def thread_target():
             self.loop.run_until_complete(self.init_fashionai(ten_env, self.app_id, self.channel, self.stream_id))
@@ -84,9 +83,13 @@ class FashionAIExtension(Extension):
         # TODO: process cmd
         if cmd_name == "flush":
             self.outdate_ts = datetime.now()
-            asyncio.run_coroutine_threadsafe(
-                self.flush(), self.loop
-            ).result(timeout=0.1)
+            try:
+                asyncio.run_coroutine_threadsafe(
+                    self.flush(), self.loop
+                ).result(timeout=0.1)
+            except Exception as e:
+                ten_env.return_result(CmdResult.create(StatusCode.ERROR), cmd)
+
             cmd_out = Cmd.create("flush")
             ten_env.send_cmd(cmd_out, lambda ten, result: logger.info("send_cmd flush done"))
         else:
@@ -103,10 +106,12 @@ class FashionAIExtension(Extension):
             return
 
         logger.info("FASHION_AI on data %s", inputText)
-        asyncio.run_coroutine_threadsafe(
-                self.queue.put((inputText, datetime.now())), self.loop
-        ).result(timeout=0.1)
-
+        try:        
+            asyncio.run_coroutine_threadsafe(
+                    self.queue.put((inputText, datetime.now())), self.loop
+            ).result(timeout=0.1)
+        except Exception as e:
+            ten_env.return_result(CmdResult.create(StatusCode.ERROR))
         # asyncio.get_event_loop().run_until_complete(self.client.send_inputText(inputText))
         logger.info("FASHION_AI send_inputText %s", inputText)
 
@@ -127,10 +132,10 @@ class FashionAIExtension(Extension):
         await self.client.stream_start(app_id, channel, stream_id)
         # asyncio.create_task(self.client.heartbeat(10))
         await self.client.render_start()
-        await self.create_task()
+        await self.async_polly_handler()
 
         
-    async def create_task(self):
+    async def async_polly_handler(self):
         while True:
             value = await self.queue.get()  # 从队列中获取值
             logger.info(f"async_polly_handler: loop fashion ai polly.{value}")
@@ -147,80 +152,3 @@ class FashionAIExtension(Extension):
         while not self.queue.empty():
             value = await self.queue.get()
             logger.info(f"Flushing value: {value}")
-
-
-class FashionAIClient:
-    def __init__(self, uri):
-        self.uri = uri
-        self.websocket = None
-
-    async def connect(self):
-        ssl_context = ssl._create_unverified_context()
-        self.websocket = await websockets.connect(self.uri, ssl=ssl_context)
-
-    async def stream_start(self, app_id, channel, stream_id):
-        await self.send_message(
-                {
-                    "request_id": "1",
-                    "service_id": "agora",
-                    "token": app_id,
-                    "channel_id": channel,
-                    "user_id": stream_id,
-                    "signal": "STREAM_START",
-                }
-            )
-
-    async def render_start(self):
-        await self.send_message(
-            {
-                "request_id": "3",
-                "service_id": "agora",
-                "signal": "RENDER_START",
-            }
-        )
-
-    async def send_inputText(self, inputText):
-        await self.send_message(
-           {
-                "request_id": "4",
-                "service_id": "agora",
-                "signal": "RENDER_CONTENT",
-                "text": inputText,
-            }
-        )
-
-
-    async def send_message(self, message):
-        if self.websocket is not None:
-            try:
-                await self.websocket.send(json.dumps(message))
-                logger.info(f"FASHION_AI Sent: {message}")
-                response = await asyncio.wait_for(self.websocket.recv(), timeout=2)
-                logger.info(f"FASHION_AI Received: {response}")
-            except websockets.exceptions.ConnectionClosedError as e:
-                logger.info(f"FASHION_AI Connection closed with error: {e}")
-                await self.reconnect()
-            except asyncio.TimeoutError:
-                logger.info("FASHION_AI Timeout waiting for response")
-        else:
-            logger.info("FASHION_AI WebSocket is not connected.")
-
-    async def close(self):
-        if self.websocket is not None:
-            await self.websocket.close()
-            logger.info("FASHION_AI WebSocket connection closed.")
-        else:
-            logger.info("FASHION_AI WebSocket is not connected.")
-
-    async def reconnect(self):
-        logger.info("FASHION_AI Reconnecting...")
-        await self.close()
-        await self.connect()
-
-    async def heartbeat(self, interval):
-        while True:
-            await asyncio.sleep(interval)
-            try:
-                await self.send_inputText("ping")
-            except websockets.exceptions.ConnectionClosedError:
-                break
