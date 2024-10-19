da7a39f3553fbf96b3fc74f42ab4ccd047a031a8
林锟锴 <linkunkai@agora.io>
2024-10-19 16:16:47 +0800

    update firestore tsdb

---
+++ agents/ten_packages/extension/tsdb_firestore/addon.py
@@ -0,0 +1,22 @@
+#
+#
+# Agora Real Time Engagement
+# Created by Wei Hu in 2024-08.
+# Copyright (c) 2024 Agora IO. All rights reserved.
+#
+#
+from ten import (
+    Addon,
+    register_addon_as_extension,
+    TenEnv,
+)
+from .extension import TSDBFirestoreExtension
+from .log import logger
+
+
+@register_addon_as_extension("tsdb_firestore")
+class TSDBFirestoreExtensionAddon(Addon):
+
+    def on_create_instance(self, ten_env: TenEnv, name: str, context) -> None:
+        logger.info("TSDBFirestoreExtensionAddon on_create_instance")
+        ten_env.on_create_instance_done(TSDBFirestoreExtension(name), context)
