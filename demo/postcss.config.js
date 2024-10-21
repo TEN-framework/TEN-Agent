bf023f841eca66d7c93f71974a55fbc7c7b7b61c
czhen <56986964+shczhen@users.noreply.github.com>
2024-10-21 11:34:26 +0800

    feat: apply new FE components
    - apply Yarn 4
    
    New packages:
    - prettier
    - tailwind

--- demo/postcss.config.js
+++ demo/postcss.config.js
@@ -1,5 +1,7 @@
+/** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
+    tailwindcss: {},
     autoprefixer: {},
     "@minko-fe/postcss-pxtoviewport": {
       viewportWidth: 375,
