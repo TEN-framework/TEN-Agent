b45cbd2d58ce49de3341ae1c7bb6476e5176e2a8
czhen <56986964+shczhen@users.noreply.github.com>
2024-11-06 04:24:59 +0800

    feat(demo): new UI & rtm user input (#357)
    * feat: apply new FE components
    
    - apply Yarn 4
    
    New packages:
    - prettier
    - tailwind
    
    * feat: add shadcn/ui
    
    * Update Dockerfile
    
    * refine login card
    
    * add layout
    
    * add rtc card
    
    * support responsive
    
    * apply new home
    
    * init rtmManager
    
    * update README
    
    * add rtm in token gen
    
    * apply rtm
    
    * add graph camera_va_openai_azure_rtm
    
    * apply rtm
    
    * refine code
    
    * chore: refine code
    
    * chore: update gitignore
    
    * feat: add rtm_wrapper
    
    * fix: rtm builder and config replace bug
    
    * Update property.json
    
    * fix: fix conflict
    
    ---------
    
    Co-authored-by: TomasLiu <liuchang@agora.io>
    Co-authored-by: chencheng <chencheng@shengwang.cn>
    Co-authored-by: Ethan Zhang <qianze.zhang@hotmail.com>
    Co-authored-by: Zhang Qianze <zhangqianze@agora.io>

---
+++ agents/ten_packages/extension/agora_rtm_wrapper/go.mod
@@ -0,0 +1,7 @@
+module agora_rtm_wrapper
+
+go 1.20
+
+replace ten_framework => ../../system/ten_runtime_go/interface
+
+require ten_framework v0.0.0-00010101000000-000000000000
--- server/internal/http_server.go
+++ server/internal/http_server.go
@@ -253,7 +253,7 @@ func (s *HttpServer) handlerGenerateToken(c *gin.Context) {
 		return
 	}
 
-	token, err := rtctokenbuilder.BuildTokenWithUid(s.config.AppId, s.config.AppCertificate, req.ChannelName, req.Uid, rtctokenbuilder.RolePublisher, tokenExpirationInSeconds, tokenExpirationInSeconds)
+	token, err := rtctokenbuilder.BuildTokenWithRtm(s.config.AppId, s.config.AppCertificate, req.ChannelName, fmt.Sprintf("%d", req.Uid), rtctokenbuilder.RolePublisher, tokenExpirationInSeconds, tokenExpirationInSeconds)
 	if err != nil {
 		slog.Error("handlerGenerateToken generate token failed", "err", err, "requestId", req.RequestId, logTag)
 		s.output(c, codeErrGenerateTokenFailed, http.StatusBadRequest)
@@ -404,7 +404,8 @@ func (s *HttpServer) processProperty(req *StartReq) (propertyJsonFile string, lo
 	// Generate token
 	req.Token = s.config.AppId
 	if s.config.AppCertificate != "" {
-		req.Token, err = rtctokenbuilder.BuildTokenWithUid(s.config.AppId, s.config.AppCertificate, req.ChannelName, 0, rtctokenbuilder.RoleSubscriber, tokenExpirationInSeconds, tokenExpirationInSeconds)
+		//req.Token, err = rtctokenbuilder.BuildTokenWithUid(s.config.AppId, s.config.AppCertificate, req.ChannelName, 0, rtctokenbuilder.RoleSubscriber, tokenExpirationInSeconds, tokenExpirationInSeconds)
+		req.Token, err = rtctokenbuilder.BuildTokenWithRtm(s.config.AppId, s.config.AppCertificate, req.ChannelName, fmt.Sprintf("%d", 0), rtctokenbuilder.RolePublisher, tokenExpirationInSeconds, tokenExpirationInSeconds)
 		if err != nil {
 			slog.Error("handlerStart generate token failed", "err", err, "requestId", req.RequestId, logTag)
 			return
