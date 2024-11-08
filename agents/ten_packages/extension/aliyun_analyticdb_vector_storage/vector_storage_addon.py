b0e61cbcb12e159c83db2a8d203d935b483966fa
Ethan Zhang <qianze.zhang@hotmail.com>
2024-11-08 11:33:07 +0800

    fix: set extension to lazy loading (#387)

--- agents/ten_packages/extension/aliyun_analyticdb_vector_storage/vector_storage_addon.py
+++ agents/ten_packages/extension/aliyun_analyticdb_vector_storage/vector_storage_addon.py
@@ -3,12 +3,12 @@ from ten import (
     register_addon_as_extension,
     TenEnv,
 )
-from .log import logger
-from .vector_storage_extension import AliPGDBExtension
 
 
 @register_addon_as_extension("aliyun_analyticdb_vector_storage")
 class AliPGDBExtensionAddon(Addon):
     def on_create_instance(self, ten: TenEnv, addon_name: str, context) -> None:
+        from .log import logger
+        from .vector_storage_extension import AliPGDBExtension
         logger.info("on_create_instance")
         ten.on_create_instance_done(AliPGDBExtension(addon_name), context)
--- agents/ten_packages/extension/deepgram_asr_python/deepgram_asr_addon.py
+++ agents/ten_packages/extension/deepgram_asr_python/deepgram_asr_addon.py
@@ -4,11 +4,11 @@ from ten import (
     TenEnv,
 )
 from .extension import EXTENSION_NAME
-from .log import logger
-from .deepgram_asr_extension import DeepgramASRExtension
 
 @register_addon_as_extension(EXTENSION_NAME)
 class DeepgramASRExtensionAddon(Addon):
     def on_create_instance(self, ten: TenEnv, addon_name: str, context) -> None:
+        from .log import logger
+        from .deepgram_asr_extension import DeepgramASRExtension
         logger.info("on_create_instance")
         ten.on_create_instance_done(DeepgramASRExtension(addon_name), context)
