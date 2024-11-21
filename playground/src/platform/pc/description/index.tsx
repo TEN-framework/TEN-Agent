6691c1ecb0ab8aea77d2e0d78da6376f27f28508
Ben Weeks <ben@agora.io>
2024-11-21 21:24:11 +0000

    improve button layout

--- playground/src/platform/pc/description/index.tsx
+++ playground/src/platform/pc/description/index.tsx
@@ -116,7 +116,8 @@ const Description = () => {
         <Select className={styles.graphName}
           disabled={agentConnected} options={graphs.map((item) => { return { label: item, value: item } })}
           value={graphName} onChange={onGraphNameChange}></Select>
-        <Button icon={<SettingOutlined />} type="primary" onClick={() => { setModal2Open(true) }}></Button>
+       
+        <Button className={styles.settinsIcon} icon={<SettingOutlined />} type="primary" onClick={() => { setModal2Open(true) }}></Button>
         {isRagGraph(graphName) ? <PdfSelect></PdfSelect> : null}
       </span>
     
