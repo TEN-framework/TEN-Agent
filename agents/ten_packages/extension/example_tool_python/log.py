db2d93de35cbc73327c2e7b367fd4ba487699cfc
Ben Weeks <ben@agora.io>
2024-11-26 15:44:35 +0000

    add example tool

---
+++ agents/ten_packages/extension/example_tool_python/log.py
@@ -0,0 +1,16 @@
+
+import logging
+
+logger = logging.getLogger("example_tool_python")
+logger.setLevel(logging.INFO)
+
+formatter_str = (
+    "%(asctime)s - %(name)s - %(levelname)s - %(process)d - "
+    "[%(filename)s:%(lineno)d] - %(message)s"
+)
+formatter = logging.Formatter(formatter_str)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
