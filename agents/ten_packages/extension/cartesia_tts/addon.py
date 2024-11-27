616c55b7e9cfcec8ebbc2ec7f4d5ec02f7346904
sunshinexcode <24xinhui@163.com>
2024-11-27 17:06:06 +0800

    refactor(): refactor cartesia tts (#444)

---
+++ agents/ten_packages/extension/cartesia_tts/addon.py
@@ -0,0 +1,19 @@
+#
+# This file is part of TEN Framework, an open source project.
+# Licensed under the Apache License, Version 2.0.
+# See the LICENSE file for more information.
+#
+from ten import (
+    Addon,
+    register_addon_as_extension,
+    TenEnv,
+)
+
+
+@register_addon_as_extension("cartesia_tts")
+class CartesiaTTSExtensionAddon(Addon):
+
+    def on_create_instance(self, ten_env: TenEnv, name: str, context) -> None:
+        from .extension import CartesiaTTSExtension
+        ten_env.log_info("CartesiaTTSExtensionAddon on_create_instance")
+        ten_env.on_create_instance_done(CartesiaTTSExtension(name), context)
