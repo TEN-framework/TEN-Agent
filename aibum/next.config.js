34fc294cfa6d2662dc55970db2fa3bafbe77b7fc
pudgeli <pudgeli@tencent.com>
2025-03-21 01:51:15 +0000

    添加镜像等逻辑

--- aibum/next.config.js
+++ aibum/next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || '',
   },
   // 如果网站部署在子路径下，可以设置basePath
   // basePath: '/your-base-path',
-  
+
   // 配置静态资源路径
   assetPrefix: process.env.NEXT_PUBLIC_BASE_URL || '',
-  
+
   // 图像优化配置
   images: {
     domains: ['memory-test.smartapetech.com'],
