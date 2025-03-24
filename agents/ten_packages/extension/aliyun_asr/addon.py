8dea9377150d9121cee0af19ab0a04a2f9868621
Jay Zhang <wangyoucao577@gmail.com>
2025-03-24 12:36:09 +0000

    style: format

--- agents/ten_packages/extension/aliyun_asr/addon.py
+++ agents/ten_packages/extension/aliyun_asr/addon.py
@@ -4,9 +4,11 @@ from ten import (
     TenEnv,
 )
 
+
 @register_addon_as_extension("aliyun_asr")
 class AliyunASRExtensionAddon(Addon):
     def on_create_instance(self, ten: TenEnv, addon_name: str, context) -> None:
         from .extension import AliyunASRExtension
+
         ten.log_info("on_create_instance")
         ten.on_create_instance_done(AliyunASRExtension(addon_name), context)
